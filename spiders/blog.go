b1cd977564a7cd15bdbfc2d406407be39f959f49
root <root@MRBIAN-WINDOWS.localdomain>
2021-09-22 00:03:04 +0800

    添加解析markdown文章

--- spiders/blog.go
+++ spiders/blog.go
@@ -5,14 +5,12 @@ import (
 	"crypto/sha256"
 	"crypto/tls"
 	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"math"
 	"math/rand"
 	"net/http"
 	Url "net/url"
-	"regexp"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -20,7 +18,7 @@ import (
 
 const (
 	blog_api               = "https://blog.csdn.net/community/home-api/v1/get-business-list?page=%d&size=20&businessType=blog&orderby=&noMore=false&username=%s"
-	blog_markdown_api      = "https://bizapi.csdn.net/blog-console-api/v3/editor/getArticle?id=%d&model_type="
+	blog_markdown_api      = "https://bizapi.csdn.net/blog-console-api/v3/editor/getArticle?id=%s&model_type="
 	blog_size              = 20
 	x_ca_key               = "203803574"
 	x_ca_signature_headers = "x-ca-key,x-ca-nonce"
@@ -39,8 +37,36 @@ type Blog struct {
 var blog_counter int = 1
 var blogs []*Blog
 var user string
+var default_header map[string]string = map[string]string{
+	"accept":                 "*/*",           //需要指定该头，不然就会报签名错误
+	"accept-encoding":        "gzip, deflate", //需要指定该头，不然就会报签名错误
+	"x-ca-key":               x_ca_key,
+	"x-ca-signature-headers": x_ca_signature_headers,
+	"x-ca-nonce":             "",
+	"x-ca-signature":         "",
+	"cookie":                 `uuid_tt_dd=10_9893908260-1631422578939-282460; _ga=GA1.2.892729514.1631631538; c_first_ref=www.google.com.hk; c_first_page=https://blog.csdn.net/Fei20140908/article/details/114849593; c_segment=12; dc_sid=8d2579a401f7d91942159c1334ac3b08; is_advert=1; _gid=GA1.2.655835131.1631807398; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1631631538,1631632180,1631807398; dc_session_id=10_1631889878007.754241; unlogin_scroll_step=1631889884892; SESSION=ba973ce6-d269-450c-b554-e6fb4127515e; ssxmod_itna=eqRxnDBDcDRD97DyDGxBpOW5t0Qqe4xBAwFehKDsq3aDpxBKidDaxQapC+PuewReerbxmwIteFguifo=sRL4GLDmKQtQUWxiiDC40rD74irDDxD3Db4KDSCxG=DjWz6MyvxYPGWAqitfDQyODiPD=2yHZxi7DD5DArPDwx7OtQ0eTWKDerasUeokCAgDqWKD9xYDsEifORAfjt2C833ERnqDUWGU3GL4qe2DSpRHDlF2DC9kUi5Qr+d8cxvza0DTFHxNYWx5E0D0/Y44xEr4eejq+sedKAD5tnGKOdVeDDcpOCOPrYD=; ssxmod_itna2=eqRxnDBDcDRD97DyDGxBpOW5t0Qqe4xBAwFexikAqK33trDlZODjR0Wn6merlW=Gs+xApxyUEB5RxRD4Q+w+5OWTxhxTL2gIW+accWka2K6zqr2fYbfzM=OIq5v8y20HbIziM42kFYeBidGU8ou0odGQwhZiCiuYRIRf4rvfrbM+WN6Y+L=tmuYvlbdNCbEFmuPVQaQNbjPh6Wf7Bi7viEv8kHsDf3KQ7iVK7ByKYd+Y7dABioKy277Z6KRQU3tj1HmcZcfp2KeZgYhKQCtoytUQ6RwW7+pt3f2Nwhn/6=7Lo6S7OI8FKA3ezD4E+vilRr7Tjtqq=YiFALz7qpYxL2ehaP542mbVUe0Wes7iyDTlfI9743L+xAKaCpiQizUx3EbQoKl05qjP+UrYPbYHacj7K=oqE+nIohfhqiGQbvtu9b/a4gqtixDKwPtB30q5MixdZTK7LeAL0fPh1I2nLsBTT7OYGxwqF4SKX1I1ehVnxMD5uYwM1I1PhG1D3BTCZX1E05z5LPhlZxlpqTY53zqr9xrzDQDDLxD20od+0dQLTi9QnPqAQV/GDD==; acw_sc__v2=6144aa7a2c1a201ae02ccf8558621979f165528e; UserName=duandianR; UserInfo=c8ec4cf1eaba4441b605b8dd54d7ec1e; UserToken=c8ec4cf1eaba4441b605b8dd54d7ec1e; UserNick=断桥bian; AU=4F0; UN=duandianR; BT=1631890051748; p_uid=U010000; c_page_id=default; Hm_up_6bcd52f51e9b3dce32bec4a3997715ac={"islogin":{"value":"1","scope":1},"isonline":{"value":"1","scope":1},"isvip":{"value":"0","scope":1},"uid_":{"value":"duandianR","scope":1}}; Hm_ct_6bcd52f51e9b3dce32bec4a3997715ac=6525*1*10_9893908260-1631422578939-282460!5744*1*duandianR; log_Id_view=16; log_Id_click=3; c_pref=https://blog.csdn.net/qq_35524157/article/details/117385786; c_ref=https://mp.csdn.net/mp_blog/manage/article?spm=1001.2101.3001.5448; dc_tos=qzl2v0; log_Id_pv=12; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1631890622`,
+}
+var header *http.Header = &http.Header{}
 
-var collector *colly.Collector = colly.NewCollector()
+var transport *http.Transport = &http.Transport{
+	TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true,
+	},
+}
+var list_collector *colly.Collector
+var markdown_collector *colly.Collector
+
+func init() {
+	for key, value := range default_header {
+		header.Add(key, value)
+	}
+	collector := colly.NewCollector()
+	collector.WithTransport(transport)
+	list_collector = collector.Clone()
+	markdown_collector = list_collector.Clone()
+	list_collector.OnResponse(parse_blog_list)
+	markdown_collector.OnResponse(parse_blog_markdown)
+}
 
 func IntRange(start int, end int, step int) []int {
 	var seq_cap int = (end - start) / step
@@ -79,23 +105,25 @@ func getSign(uuid string, url string) string {
 		panic(fmt.Sprintf("Failed to parse url %s (%s)", url, err))
 	}
 	ekey := []byte("9znpamsyl2c7cdrr9sas0le9vbc3r6ba")
-	fmt.Println("ekey: ", ekey)
 	url_query := u_url.RawQuery
 	url_compnent := u_url.Path + "?" + url_query[0:len(url_query)-1]
 	url_str := fmt.Sprintf("GET\n*/*\n\n\n\nx-ca-key:203803574\nx-ca-nonce:%s\n%s", uuid, url_compnent)
-	fmt.Println("url_str: ", url_str)
 	to_enc := []byte(url_str)
-	fmt.Println("to_enc: ", to_enc)
 	hmac_encoder := hmac.New(sha256.New, ekey)
 	hmac_encoder.Write(to_enc)
 	hmac_digest := hmac_encoder.Sum(nil)
-	fmt.Println("hmac_hex_digest: ", hex.EncodeToString(hmac_digest))
-	fmt.Println("hmac_digest: ", hmac_digest)
 	sign := base64.StdEncoding.EncodeToString(hmac_digest)
 	return sign
 }
 
 func crawl_blog_markdown(article_id string) {
+	blog_markdown_url := fmt.Sprintf(blog_markdown_api, article_id)
+	uuid := createUuid()
+	sign := getSign(uuid, blog_markdown_url)
+	header.Set("x-ca-nonce", uuid)
+	header.Set("x-ca-signature", sign)
+	fmt.Println(fmt.Sprintf("Crawl markdown for blog: %s  ... ", blog_markdown_url))
+	markdown_collector.Request("GET", blog_markdown_url, nil, nil, *header)
 }
 
 func parse_blog_list(resp *colly.Response) {
@@ -129,6 +157,7 @@ func parse_blog_list(resp *colly.Response) {
 		zblog.comment = int(vblog["commentCount"].(float64))
 		zblog.view = int(vblog["viewCount"].(float64))
 		zblog.createTime = postTime
+		crawl_blog_markdown(zblog.id)
 		blogs = append(blogs, zblog)
 		blog_counter += 1
 	}
@@ -137,82 +166,30 @@ func parse_blog_list(resp *colly.Response) {
 		total_blog_page := int(math.Ceil(float64(blog_total) / blog_size))
 		blog_url := fmt.Sprintf(blog_api, blog_page, user)
 		fmt.Println(fmt.Sprintf("Crawl user %s blogs ... at page %d/%d", user, blog_page, total_blog_page))
-		collector.Visit(blog_url)
+		list_collector.Visit(blog_url)
 	}
 }
 
 func parse_blog_markdown(resp *colly.Response) {
-
-}
-
-func parseResponse(resp *colly.Response) error {
-	blog_md_reg, blog_md_reg_err := regexp.Compile(`https?:\/\/blog\.csdn\.net\/community\/home-api\/v1\/get-business-list\?.*`)
-	blog_list_reg, blog_list_reg_err := regexp.Compile(`https?:\/\/bizapi\.csdn\.net\/blog-console-api\/v3\/editor\/getArticle\?.*`)
-	url := resp.Request.URL.String()
-	if blog_md_reg_err != nil {
-		return fmt.Errorf("Failed to compilre regex of blog markdown url regex.")
-	} else if blog_list_reg_err != nil {
-		return fmt.Errorf("Failed to compilre regex of blog list url regex.")
+	if resp.StatusCode != 200 {
+		fmt.Printf("Crawl markdown article %s failed!\n", resp.Request.URL)
+		return
 	}
-	switch {
-	case blog_list_reg.MatchString(url):
-		parse_blog_list(resp)
-	case blog_md_reg.MatchString(url):
-		parse_blog_markdown(resp)
-	}
-	return nil
+	var resp_json map[string]interface{}
+	json.Unmarshal(resp.Body, &resp_json)
+	fmt.Println(resp_json)
 }
 
 func crawl_blog(user string) []*Blog {
-	var blog_counter int = 1
 	blog_url := fmt.Sprintf(blog_api, blog_counter, user)
-	var blogs []*Blog
-	collector.Visit(blog_url)
-	collector.Wait()
+	list_collector.Visit(blog_url)
+	list_collector.Wait()
+	markdown_collector.Wait()
 	return blogs
 }
 
 func main() {
-	//blog_user := "duandianR"
-	//crawl_blog(blog_user)
-	//
-	//collector.OnResponse(parseResponse)
+	blog_user := "duandianR"
+	crawl_blog(blog_user)
 
-	uuid := createUuid() //"442584db-e5c5-4d3b-6528-3d3c19f2ffa3"
-	url := "https://bizapi.csdn.net/blog-console-api/v3/editor/getArticle?id=80341748&model_type="
-	fmt.Println("uuid: ", uuid)
-	fmt.Println("url: ", url)
-	sign := getSign(uuid, url)
-	fmt.Println("sign: ", sign)
-	var headers map[string]string = map[string]string{
-		"accept":                 "*/*", //需要该头
-		"accept-encoding":        "gzip, deflate",
-		"x-ca-key":               x_ca_key,
-		"x-ca-signature-headers": x_ca_signature_headers,
-		"x-ca-nonce":             uuid,
-		"x-ca-signature":         sign,
-		"cookie":                 `uuid_tt_dd=10_9893908260-1631422578939-282460; _ga=GA1.2.892729514.1631631538; c_first_ref=www.google.com.hk; c_first_page=https://blog.csdn.net/Fei20140908/article/details/114849593; c_segment=12; dc_sid=8d2579a401f7d91942159c1334ac3b08; is_advert=1; _gid=GA1.2.655835131.1631807398; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1631631538,1631632180,1631807398; dc_session_id=10_1631889878007.754241; unlogin_scroll_step=1631889884892; SESSION=ba973ce6-d269-450c-b554-e6fb4127515e; ssxmod_itna=eqRxnDBDcDRD97DyDGxBpOW5t0Qqe4xBAwFehKDsq3aDpxBKidDaxQapC+PuewReerbxmwIteFguifo=sRL4GLDmKQtQUWxiiDC40rD74irDDxD3Db4KDSCxG=DjWz6MyvxYPGWAqitfDQyODiPD=2yHZxi7DD5DArPDwx7OtQ0eTWKDerasUeokCAgDqWKD9xYDsEifORAfjt2C833ERnqDUWGU3GL4qe2DSpRHDlF2DC9kUi5Qr+d8cxvza0DTFHxNYWx5E0D0/Y44xEr4eejq+sedKAD5tnGKOdVeDDcpOCOPrYD=; ssxmod_itna2=eqRxnDBDcDRD97DyDGxBpOW5t0Qqe4xBAwFexikAqK33trDlZODjR0Wn6merlW=Gs+xApxyUEB5RxRD4Q+w+5OWTxhxTL2gIW+accWka2K6zqr2fYbfzM=OIq5v8y20HbIziM42kFYeBidGU8ou0odGQwhZiCiuYRIRf4rvfrbM+WN6Y+L=tmuYvlbdNCbEFmuPVQaQNbjPh6Wf7Bi7viEv8kHsDf3KQ7iVK7ByKYd+Y7dABioKy277Z6KRQU3tj1HmcZcfp2KeZgYhKQCtoytUQ6RwW7+pt3f2Nwhn/6=7Lo6S7OI8FKA3ezD4E+vilRr7Tjtqq=YiFALz7qpYxL2ehaP542mbVUe0Wes7iyDTlfI9743L+xAKaCpiQizUx3EbQoKl05qjP+UrYPbYHacj7K=oqE+nIohfhqiGQbvtu9b/a4gqtixDKwPtB30q5MixdZTK7LeAL0fPh1I2nLsBTT7OYGxwqF4SKX1I1ehVnxMD5uYwM1I1PhG1D3BTCZX1E05z5LPhlZxlpqTY53zqr9xrzDQDDLxD20od+0dQLTi9QnPqAQV/GDD==; acw_sc__v2=6144aa7a2c1a201ae02ccf8558621979f165528e; UserName=duandianR; UserInfo=c8ec4cf1eaba4441b605b8dd54d7ec1e; UserToken=c8ec4cf1eaba4441b605b8dd54d7ec1e; UserNick=断桥bian; AU=4F0; UN=duandianR; BT=1631890051748; p_uid=U010000; c_page_id=default; Hm_up_6bcd52f51e9b3dce32bec4a3997715ac={"islogin":{"value":"1","scope":1},"isonline":{"value":"1","scope":1},"isvip":{"value":"0","scope":1},"uid_":{"value":"duandianR","scope":1}}; Hm_ct_6bcd52f51e9b3dce32bec4a3997715ac=6525*1*10_9893908260-1631422578939-282460!5744*1*duandianR; log_Id_view=16; log_Id_click=3; c_pref=https://blog.csdn.net/qq_35524157/article/details/117385786; c_ref=https://mp.csdn.net/mp_blog/manage/article?spm=1001.2101.3001.5448; dc_tos=qzl2v0; log_Id_pv=12; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1631890622`,
-	}
-	http_proxy := func(_req *http.Request) (*Url.URL, error) {
-		return Url.Parse("http://192.168.10.4:8888")
-	}
-	http_transport := &http.Transport{
-		Proxy: http_proxy,
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
-	client := &http.Client{
-		Transport: http_transport,
-	}
-	req, _ := http.NewRequest("GET", url, nil)
-	for key, value := range headers {
-		req.Header.Add(key, value)
-	}
-	//fmt.Println(req.Header)
-	resp, _ := client.Do(req)
-	fmt.Println(resp)
-	//fmt.Println(resp.Header)
-	//fmt.Println(resp.ContentLength)
-	//fmt.Println(err)
 }
